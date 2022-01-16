d99167d685cf609eca01fa4389dff9e49636a926
James Rowe <jroweboy@gmail.com>
2022-01-16 03:15:19 -0500

    build: Hopefully working linux static build

--- cmake/buildSDL2.cmake
+++ cmake/buildSDL2.cmake
@@ -24,9 +24,10 @@ FetchContent_GetProperties(SDL2)
 if (NOT sdl2_POPULATED)
   FetchContent_Populate(SDL2)
   set(SDL_STATIC_PIC ON CACHE BOOL "" FORCE)
-  # Turn ON SDL_SHARED so when SDL_Mixer can target it
-  # (it looks for the shared target only right now)
-  set(SDL_SHARED ON CACHE BOOL "" FORCE)
+  # Turn OFF SDL_SHARED so it doesn't set the SDL2::SDL2 target.
+  # SDL2Mixer looks for the shared target, so we create an alias
+  # for that to force it to run static instead
+  set(SDL_SHARED OFF CACHE BOOL "" FORCE)
   set(MESSAGE_QUIET OFF)
   add_subdirectory(${sdl2_SOURCE_DIR} ${sdl2_BINARY_DIR})
   unset(MESSAGE_QUIET)
@@ -35,22 +36,21 @@ endif ()
 FetchContent_Declare(
   SDL2Mixer
   GIT_REPOSITORY "https://github.com/libsdl-org/SDL_mixer.git"
-  GIT_TAG master
+  GIT_TAG 60a82b21bdbf2f4626eebee11600f29cebbe1c49
 )
 
 FetchContent_GetProperties(SDL2Mixer)
 
 if (NOT sdl2mixer_POPULATED)
   FetchContent_Populate(SDL2Mixer)
+  # 
   set(SUPPORT_OGG ON CACHE BOOL "" FORCE)
+
+  # prevent SDL2Mixer from creating its own jank SDL2::SDL2 target
+  add_library(SDL2::SDL2 ALIAS SDL2-static)
+
   set(MESSAGE_QUIET OFF)
   add_subdirectory(${sdl2mixer_SOURCE_DIR} ${sdl2mixer_BINARY_DIR})
   unset(MESSAGE_QUIET)
-
-  # hacky way to remove shared libaries from the final linker command.
-  get_target_property(EXE_LINKED_LIBS SDL2_mixer LINK_LIBRARIES)
-  list(FILTER EXE_LINKED_LIBS EXCLUDE REGEX "SDL2::SDL2")
-  list(APPEND EXE_LINKED_LIBS SDL2-static)
-  set_property(TARGET SDL2_mixer PROPERTY LINK_LIBRARIES ${EXE_LINKED_LIBS})
 
 endif ()
