13d70e8a042459e56158edb20c817ad535471dfc
James Rowe <jroweboy@gmail.com>
2022-02-13 15:09:42 -0500

    Working on making a tiled plugin

---
+++ example/assets/maps/scratch/crystalis_town_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="crystalis_town_tileset" tilewidth="8" tileheight="8" tilecount="256" columns="16">
+ <image source="crystalis_town_tileset.bmp" width="128" height="128"/>
+</tileset>
