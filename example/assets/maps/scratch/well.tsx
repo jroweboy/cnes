cbf8b182314ec5450a242f6c3fbe05952ea1aeea
James Rowe <jroweboy@gmail.com>
2022-02-17 22:27:46 -0500

    Small tileset work for tiled-nes

--- example/assets/maps/scratch/well.tsx
+++ example/assets/maps/scratch/well.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="well" tilewidth="8" tileheight="8" tilecount="4" columns="2">
+<tileset version="1.8" tiledversion="1.8.1" name="well" tilewidth="8" tileheight="8" tilecount="4" columns="2">
  <image source="../well.tmx" width="16" height="16"/>
 </tileset>
